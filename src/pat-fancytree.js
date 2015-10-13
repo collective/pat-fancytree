c36f06f4ebb86a27e97ffaae927b12fb2031607d
Jens W. Klein <jk@kleinundpartner.at>
2015-10-13 15:37:36 +0300

    make pattenr options work

--- src/pat-fancytree.js
+++ src/pat-fancytree.js
@@ -5,35 +5,12 @@ define([
     "pat-parser",
     "pat-logger",
     "fancytree"
-], function(Base, registry, Parser, logger) {
+], function(Base, registry, Parser) {
     "use strict";
 
-    var log = logger.getLogger("pat-fancytree");
-    /* For logging, you can call log.debug, log.info, log.warn, log.error and log.fatal.
-     *
-     * For more information on how to use the logger and how to view log messages, please read:
-     * https://github.com/Patternslib/logging
-     */
-
     var parser = new Parser("fancytree");
-    /* If you"d like your pattern to be configurable via the
-     * data-pat fancytree attribute, then you need to
-     * specify the available arguments here, by calling parser.addArgument.
-     *
-     * The addArgument method takes the following parameters:
-     *  - name: The required name of the pattern property which you want to make
-     *      configurable.
-     *  - default_value: An optional default string value of the property if the user
-     *      doesn"t provide one.
-     *  - choices: An optional set (Array) of values that the property might take.
-     *      If specified, values outside of this set will not be accepted.
-     *  - multiple: An optional boolean value which specifies wether the
-     *      property can be multivalued or not.
-     *
-     *  For example:
-     *      parser.addArgument("color", "blue", ["red", "green", "blue"], false);
-     */
-    parser.addArgument("selectMode", "2", ["1", "2", "3"]);
+    parser.addArgument("selectMode", 2, [1, 2, 3]);
+    parser.addArgument("checkbox", false);
 
     return Base.extend({
         /* The name is used to store the pattern in a registry and needs to be
@@ -46,14 +23,10 @@ define([
         trigger: ".pat-fancytree",
 
         init: function initUndefined () {
-            this.options = parser.parse(this.$el);
-            /* this.options will now contain the configured pattern properties
-             * you"ve registered with the parser.addArgument method.
-             *
-             * If the user provided any values via the data-pat fancytree
-             * attribute, those values will already be set.
-             */
-             this.$el.fancytree({ selectMode: this.options.selectMode });
+          console.log("selectMode " + this.options.selectMode);
+          console.log("checkbox " + this.options.checkbox);
+          this.options = parser.parse(this.$el);
+          this.$el.fancytree(this.options);
         }
     });
 });
